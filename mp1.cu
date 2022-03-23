3fb95d826021d19a1430f89fb826acd98932fb19
gradypalfrey <gradypalfrey@gmail.com>
2022-03-22 20:51:53 -0700

    Add mp1

--- mp1.cu
+++ mp1.cu
@@ -1,3 +1,4 @@
+
 #include "cuda_runtime.h"
 
 #include <string.h> 
@@ -6,20 +7,6 @@
 #include <iostream>
 #include <memory>
 #include <string>
-
-__global__ void increment_kernel(int *g_data, int inc_value)
-{
-	int idx = blockIdx.x * blockDim.x + threadIdx.x;
-	g_data[idx] = g_data[idx] + inc_value;
-}
-
-int correct_output(int *data, const int n, const int x)
-{
-	for (int i = 0; i < n; i++)
-		if (data[i] != x)
-			return 0;
-	return 1;
-}
 
 int main(int argc, char *argv[])
 {
@@ -46,8 +33,7 @@ int main(int argc, char *argv[])
 			deviceProp.clockRate * 1e-6f);
 		printf("  Total number of streaming multiprocessors:        %zu bytes\n",
 			deviceProp.multiProcessorCount);
-		printf("  Total number of cores:        %03d cores\n",
-			_ConvertSMVer2Cores(deviceProp.major, deviceProp.minor) * deviceProp.multiProcessorCount);
+		printf("  Total number of cores:        cores\n");
 		printf("  Total amount of constant memory:               %zu bytes\n",
 			deviceProp.totalConstMem);
 		printf("  Total amount of shared memory per block:       %zu bytes\n",
@@ -65,64 +51,5 @@ int main(int argc, char *argv[])
 			deviceProp.maxGridSize[0], deviceProp.maxGridSize[1],
 			deviceProp.maxGridSize[2]);
 	}
-
-	int n = 16 * 1024 * 1024;
-	int nbytes = n * sizeof(int);
-	int value = 26;
-
-	// allocate host memory 
-	int *a = 0;
-	cudaMallocHost((void**)&a, nbytes);
-	//    memset(a, 0, nbytes); 
-
-	// allocate device memory 
-	int *d_a = 0;
-	cudaMalloc((void**)&d_a, nbytes);
-	cudaMemset(d_a, 255, nbytes);
-	// set kernel launch configuration 
-	dim3 threads = dim3(512, 1);
-	dim3 blocks = dim3(n / threads.x, 1);
-
-	// create cuda event handles 
-	cudaEvent_t start, stop;
-	cudaEventCreate(&start);
-	cudaEventCreate(&stop);
-
-	cudaDeviceSynchronize();
-	float gpu_time = 0.0f;
-
-	// asynchronously issue work to the GPU (all to stream 0) 
-	cudaEventRecord(start, 0);
-	cudaMemcpyAsync(d_a, a, nbytes, cudaMemcpyHostToDevice, 0);
-	increment_kernel <<<blocks, threads, 0, 0 >> > (d_a, value);
-	cudaMemcpyAsync(a, d_a, nbytes, cudaMemcpyDeviceToHost, 0);
-	cudaEventRecord(stop, 0);
-
-	// have CPU do some work while waiting for GPU to finish 
-	unsigned long int counter = 0;
-	while (cudaEventQuery(stop) == cudaErrorNotReady)
-	{
-		counter++; // Indicates that the CPU is running asynchronously while GPU is executing  
-	}
-
-	cudaEventSynchronize(stop);   // stop is updated here 
-	cudaEventElapsedTime(&gpu_time, start, stop);   //time difference between start and stop 
-
-	   // print the GPU times 
-	printf("time spent executing by the GPU: %.2f\n", gpu_time);
-	printf("CPU executed %d iterations while waiting for GPU to finish\n", counter);
-
-	// check the output for correctness 
-	bool bFinalResults = (bool)correct_output(a, n, value);
-
-
-
-	// release resources 
-	cudaEventDestroy(start);
-	cudaEventDestroy(stop);
-	cudaFreeHost(a);
-	cudaFree(d_a);
-	cudaDeviceReset();
-
 	return 0;
 }
